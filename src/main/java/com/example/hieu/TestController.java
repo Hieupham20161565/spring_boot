186cdd5b00dbb1f683732e0e241925f2971cc1e8
cuong <cuong.nv5@cmctelecom.vn>
2021-07-06 11:15:13 +0700

    skcahckoascksnc

--- src/main/java/com/example/hieu/TestController.java
+++ src/main/java/com/example/hieu/TestController.java
@@ -8,36 +8,36 @@ import java.io.*;
 @RestController()
 @RequestMapping("index")
 public class TestController {
+
+
     @GetMapping("/")
     public String index(String a)  {
 //        File file = new File("")
-        try {
-            File file =   ResourceUtils.getFile("/src/main/resources/data.dontfile.txt");
-            try (FileWriter fw = new FileWriter(file);
-                 BufferedWriter bw = new BufferedWriter(fw)) {
-                bw.write("cccadsa");
-                bw.newLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+try {
+
+    File file =   ResourceUtils.getFile("/src/main/resources/data.dontfile.txt");
+    FileWriter fw = new FileWriter(file);
+    BufferedWriter bw = new BufferedWriter(fw);
+    bw.write("ahghfgfg");
+    bw.newLine();
+    FileReader fr = new FileReader(file);
+    BufferedReader br = new BufferedReader(fr);
+    String line;
+    while((line = br.readLine()) != null){
+        System.out.print(line);
+    }
+
+
+} catch (FileNotFoundException e) {
+    e.printStackTrace();
+} catch (IOException e) {
+    e.printStackTrace();
+}
+
         return a;
+
+
+
     }
 
-    @RequestMapping(value = "index2",method = RequestMethod.GET)
-    public String index2(){
-        return "hello 2";
-    }
-
-    @PostMapping("testPort")
-    public String testPost(String a){
-        return a;
-    }
-
-    @PostMapping("portClassTest")
-    public Test portClass(@RequestBody Test test){
-        return test;
-                                                                                                                                                          }
 }
